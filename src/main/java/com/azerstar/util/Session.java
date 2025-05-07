5a470caef5e17d38470d3224dfda567108497602
Elvin Semedzade <Elvin.Semedzade@almarket.az>
2025-05-07 17:25:23 +0400

    update

--- src/main/java/com/azerstar/util/Session.java
+++ src/main/java/com/azerstar/util/Session.java
@@ -9,14 +9,28 @@ public class Session {
     public static void setCurrentUsername(String username) {
         currentUsername = username;
     }
+    public static void setCurrentFirstname(String firstname) {
+        currentFirstname = firstname;
+    }
+    public static void setCurrentLastname(String lastname) {
+        currentLastname = lastname;
+    }
 
 
     public static String getCurrentUsername() {
         return currentUsername;
     }
+    public static String getCurrentFirstname() {
+        return currentFirstname;
+    }
+    public static String getCurrentLastname() {
+        return currentLastname;
+    }
 
     public static void clearSession() {
         currentUsername = null;
+        currentFirstname = null;
+        currentLastname = null;
 
     }
 }
